4d8a3415dbeed826440ebf7ebed52237c5639555
Denis Ibanez <denis.ibanez@outlook.com>
2020-06-27 20:22:58 +0800

    footer and client section

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  items = [
+  public slideItems: any
+  public items = [
     { url: '../../../assets/bullseye-arrow.svg', title: 'Gerenciamento de Metas', text:'A chave para o crescimento das vendas começa com uma estratégia bem desenvolvida junto aos parceiros, com objetivos claros e bem definidos de vendas, personalizados para cada um.' },
     { url: '../../../assets/chart-bar.svg', title: 'Faturamentos & KPIs', text:'Municie sua equipe e seus clientes com informações atualizadas diariamente para acompanhamento do faturamento, KPIs e informações gerenciais por período, segmento, linha de produtos.' },
     { url: '../../../assets/file-signature.svg', title: 'Acordos Comerciais', text:'Fortaleça o relacionamento com seus parceiros de negócio formalizando os objetivos a serem atingidos e os benefícios para recompensá-los.' },
@@ -14,9 +15,16 @@ export class HomeComponent implements OnInit {
     { url: '../../../assets/hands-usd.svg', title: 'Gestão de Bonificação', text:'Bonifique os parceiros comprometidos com os objetivos da corporação e que estejam trazendo bons resultados.' },
     { url: '../../../assets/badge-percent.svg', title: 'Ações Promocionais', text:'Crie e acompanhe ações promocionais com o auxílio de ferramentas intuitivas que permitem os usuários alocarem gastos previamente aprovados, garantindo uma execução precisa e consistente.' }
   ]
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.slideItems = [
+      { src: '../../../assets/Logo.Schneider.svg', title: 'Title 1' },
+      { src: '../../../assets/Logo.Schneider.svg', title: 'Title 2' },
+      { src: '../../../assets/Logo.Schneider.svg', title: 'Title 3' },
+      { src: '../../../assets/Logo.Schneider.svg', title: 'Title 4' },
+      { src: '../../../assets/Logo.Schneider.svg', title: 'Title 5' }
+    ];
+  }
 }
